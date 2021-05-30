7ebaa5d043ccb417469f03139354dbaf4e574845
jonathan03062011 <76205715+jonathan03062011@users.noreply.github.com>
2021-05-30 15:18:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+ canvas=createCanvas(500,500);
+ canvas.position(550,100);
+ video=createCapture(VIDEO);
+ video.size(500,500);
+}
+
+function draw()
+{
+background("#ff9500");
+}
